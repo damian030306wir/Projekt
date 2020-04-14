04bde9c1fe7caf43d787b323f6cfcfb85e0c357a
Bartosz Michalski <bartek10114@wp.pl>
2020-04-14 17:19:51 +0200

    dodanie pinow poprzez przycisk

--- Projekt/Projekt/Mapa.xaml.cs
+++ Projekt/Projekt/Mapa.xaml.cs
@@ -4,6 +4,11 @@ using Xamarin.Forms.Xaml;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
 
 namespace Projekt
 {
@@ -11,16 +16,29 @@ namespace Projekt
     public partial class Mapa : ContentPage
     {
         public ObservableCollection<Position> positions = new ObservableCollection<Position>();
-        public Mapa() 
-        {   
-            InitializeComponent();
-        }
-        private void OnMapClicked(object sender, MapClickedEventArgs e)
+        public ObservableCollection<Position> piny = new ObservableCollection<Position>();
+
+        public Mapa()
         {
+            InitializeComponent();
+
+        }
+        void ListaPunktowPage2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+        void DodajZdjecie(object sender, EventArgs e)
+        {
+            DisplayAlert("Sukces", "Obrazek został dodany pomyślnie", "Anuluj");
+        }
+        public void OnMapClicked(object sender, MapClickedEventArgs e)
+        {
+
             Pin pin = new Pin
             {
-                Label = "Punkt",
-                Address = "Adres",
+
+                Label = "Nazwa",
+                Address = "adres",
                 Type = PinType.SavedPin,
                 Position = new Position(e.Position.Latitude, e.Position.Longitude)
             };
@@ -33,11 +51,54 @@ namespace Projekt
             foreach (Position pos in positions)
             {
                 polyline.Geopath.Add(pos);
+                
             }
             maps.Pins.Add(pin);
             maps.MapElements.Add(polyline);
+
+
         }
 
+        private async void OnButton(object sender, EventArgs e) 
+        {
 
+
+            Pin pin = new Pin
+            {
+
+                Label = "Nazwa",
+                Address = "adres",
+                Type = PinType.SavedPin,
+                Position = new Position(Convert.ToDouble(szerokosc.Text), Convert.ToDouble(dlugosc.Text))
+
+            };
+            piny.Add(new Position(Convert.ToDouble(szerokosc.Text), Convert.ToDouble(dlugosc.Text)));
+            Polyline polyline = new Polyline
+            {
+                StrokeColor = Color.Blue,
+                StrokeWidth = 6
+            };
+            foreach (Position pos in piny)
+            {
+                polyline.Geopath.Add(pos);
+
+            }
+            maps.Pins.Add(pin);
+            maps.MapElements.Add(polyline);
+
+            var json = JsonConvert.SerializeObject(new { X = pin.Position.Latitude, Y = pin.Position.Longitude });
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpClient client = new HttpClient();
+
+            var result = await client.PostAsync("http://dearjean.ddns.net:44301/api/Points", content);
+
+            if (result.StatusCode == HttpStatusCode.Created)
+            {
+                await DisplayAlert("Komunikat", "Dodanie puntku przebiegło pomyślnie", "Anuluj");
+            }
+           
+        }
     }
 }
--- Projekt/Projekt/Punkty.cs
+++ Projekt/Projekt/Punkty.cs
@@ -2,13 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms.Maps;
 
 namespace Projekt
 {
-    public class Punkty
+    public class Punkty 
     {
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+        
         public string Nazwa { get; set; }
         public string Zagadka { get; set; }
         public string Odpowiedz { get; set; }
